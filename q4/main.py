d8a23f0d63ba9320534687d5daa8928f74b6818f
Kauã Lira <programakaua2025gmail.com>
2025-09-29 17:24:14 -0300

    Mais questoes resolvidas:q3 e q4

--- q4/main.py
+++ q4/main.py
@@ -1,8 +1,11 @@
 # Questão 4 - Verificador de Números Pares e Ímpares
 # Crie uma função que receba um número inteiro e retorne se ele é par ou ímpar.
+def indentificador (number):
+    if numero % 2 == 0:
+        print("Par")
+    else:
+        print("Ímpar")
 
-numero = int(input("Digite um número: "))
-if numero % 2 == 0:
-    print("Par")
-else:
-    print("Ímpar")
+numero = int(input("Digite um número: "))   
+identificar = indentificador(numero)
+print(identificar)
---
+++ q5/notas.py
@@ -0,0 +1,9 @@
+def maior(notaMa)
+
+def media(notaM)
+quantidade = 0
+    for i in notaM:
+        soma = soma + notaM 
+        quantidade = quantidade + 1
+    media = soma / quantidade
+def menor(notaMe)
